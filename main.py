a05de8d655d1053bb6fef80d81081e72c24b61d9
DamianDuque <damianduquel@gmail.com>
2025-05-05 16:41:03 -0500

    feat: microservice set up

---
+++ main.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI
+from routers.send_emails import router as email_router
+
+app = FastAPI(title="Notifications Microservice")
+app.include_router(email_router, prefix="/notifications", tags=["Notifications"])
