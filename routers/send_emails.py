b842fab7ba583fbd65d288389d7840d0a0c3b0a0
Damian <damianduquel@gmail.com>
2025-05-06 21:26:52 -0500

    fix: notification email error

--- routers/send_emails.py
+++ routers/send_emails.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter
 from pydantic import BaseModel, EmailStr
 from services.mail_service import send_welcome_email
+from fastapi import HTTPException
+import smtplib
+from email.mime.text import MIMEText
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 router = APIRouter()
 
@@ -8,7 +14,13 @@ class EmailRequest(BaseModel):
     email: EmailStr
     name: str
 
+
 @router.post("/send")
-async def send_email(data: EmailRequest):
-    await send_welcome_email(data.email, data.name)
-    return {"message": f"Correo enviado a {data.email}"}
+def send_email(data: EmailRequest):
+
+    try:
+        send_welcome_email(data.email, data.name)
+            
+        return {"message": f"Correo enviado a {data.email}"}
+    except Exception as e:
+        raise HTTPException(status_code=502, detail=str(e))
--- services/mail_service.py
+++ services/mail_service.py
@@ -2,19 +2,20 @@ import smtplib
 from email.mime.text import MIMEText
 from utils.logger import logger
 from fastapi import HTTPException
-
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
 load_dotenv()
 
-SENDER = os.getenv("SENDER")
-PASSWORD = os.getenv("PASSWORD")
+
+SENDER = os.getenv("SENDER_EMAIL")
+PASSWORD = os.getenv("SENDER_PASSWORD")
 SMTP = os.getenv("SMTP")
 PORT = os.getenv("PORT")
 
 
-async def send_welcome_email(email: str, name: str):
+
+def send_welcome_email(email: str, name: str):
     subject = "Bienvenido al Operador Oreo"
     body = f"Hola {name},\n\nTu cuenta ha sido creada exitosamente dentro del operador Oreo.\n\nBienvenido!!\nEquipo Oreo"
 
@@ -25,10 +26,12 @@ async def send_welcome_email(email: str, name: str):
 
     try:
         with smtplib.SMTP(SMTP, PORT) as server:
+        
             server.starttls()
             server.login(SENDER, PASSWORD)
             server.sendmail(SENDER, [email], msg.as_string())
         logger.info(f"Correo enviado a {email}")
+        return
     except Exception as e:
         logger.error(f"⛔ Error enviando correo a {email}: {e}")
         raise HTTPException(status_code=502, detail="No se pudo enviar el correo de bienvenida.")
